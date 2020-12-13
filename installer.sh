7bbd1b9436c63f88a298a02af8e6f8a411444a62
Flyguy86 <BJGarcia@gmail.com>
2020-12-12 20:54:58 -0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -105,12 +105,12 @@ warn "Changes are needed to the /etc/network/interfaces file"
 info "If you have modified the network on the host manualy, those can now be overwritten"
 info "If you do not overwrite this now you need to manually adjust it later"
 info "Do you want to proceed with that? [N/y] "
-read answer < /dev/tty
+#read answer < /dev/tty
 
-if [[ "$answer" =~ "y" ]] || [[ "$answer" =~ "Y" ]]; then
-    info "Replacing /etc/network/interfaces"
-    curl -sL "${URL_INTERFACES}" > "${FILE_INTERFACES}";
-fi
+#if [[ "$answer" =~ "y" ]] || [[ "$answer" =~ "Y" ]]; then
+#    info "Replacing /etc/network/interfaces"
+#    curl -sL "${URL_INTERFACES}" > "${FILE_INTERFACES}";
+#fi
 
 info "Restarting NetworkManager"
 systemctl restart "${SERVICE_NM}"
