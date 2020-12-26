babe336ad7cd19a3a8b78a70764da8166cc1ac2c
Flyguy86 <BJGarcia@gmail.com>
2020-12-26 14:47:11 -0800

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -10,6 +10,11 @@ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 # Install dependencies for HomeAssistant
 apt-get install -y apt-utils software-properties-common apparmor-utils apt-transport-https ca-certificates curl dbus jq 
 
+## Wait for APT install package manager to release lock
+while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+    echo Waiting for other instances of apt to complete...
+    sleep 5
+done
 
 # Check if Modem Manager is enabled
 if cat /sys/firmware/devicetree/base/model | grep '3' > /dev/null 2>&1; then
@@ -18,10 +23,15 @@ elif cat /sys/firmware/devicetree/base/model | grep '4' > /dev/null 2>&1; then
     echo "raspberrypi4" > ~/machine
 fi
 
-
 # Install Docker
 curl -fsSL get.docker.com > /var/lib/dietpi/postboot.d/DockerInstaller.sh
 # Hass.io supervisor installer
 curl -sL https://raw.githubusercontent.com/Flyguy86/supervised-installer/master/installer.sh > /var/lib/dietpi/postboot.d/HomeSupervisorInstaller.sh
 
+## Wait for APT install package manager to release lock
+while sudo fuser /var/{lib/{dpkg,apt/lists},cache/apt/archives}/lock >/dev/null 2>&1; do
+    echo Waiting for other instances of apt to complete...
+    sleep 5
+done
+sleep 5
 reboot
